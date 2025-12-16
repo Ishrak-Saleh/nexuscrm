0931a5e9ee8b73b0843eaa3b8b08296b9b079a50
ishrak-saleh <ischy2003@gmail.com>
2025-12-16 21:18:47 +0600

    Set up database and models: - Created Client and Note model with migration and edited their databases

---
+++ app/Models/Client.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'phone',
+        'company',
+        'address',
+        'next_follow_up',
+        'status'
+    ];
+
+    protected $casts = [
+        'next_follow_up' => 'date',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function notes(): HasMany
+    {
+        return $this->hasMany(Note::class)->latest();
+    }
+}
